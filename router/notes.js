755a5df79b82bd7ce7fcaf0db9665ae1d4115a4c
Luis Vazquez <luvazpa@gmail.com>
2021-04-06 21:22:22 -0500

    middleware reanamed

--- router/notes.js
+++ router/notes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const NotesServices = require('../services/notes');
 const boom = require('@hapi/boom');
-const passport = require('passport');
-const authErrorHandler = require('../middlewares/authHandler');
+const authErrorHandler = require('../middlewares/authErrorHandler');
 
 require('../utils/auth/strategies/jwt');
 // eslint-disable-next-line space-before-function-paren
@@ -22,71 +21,55 @@ function apiNotes(app) {
       next(boom.badRequest());
     }
   });
-  router.get(
-    '/:id',
-    passport.authenticate('jwt', { session: false }),
-    async (req, res, next) => {
-      const { id } = req.params;
-      try {
-        const note = await notesService.getNote(id);
-        res.json({
-          message: 'Note retrieved',
-          data: note
-        });
-      } catch (error) {
-        next(boom.badRequest(error));
-      }
+  router.get('/:id', authErrorHandler, async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const note = await notesService.getNote(id);
+      res.json({
+        message: 'Note retrieved',
+        data: note
+      });
+    } catch (error) {
+      next(boom.badRequest(error));
     }
-  );
-  router.post(
-    '/',
-    passport.authenticate('jwt', { session: false }),
-    async (req, res, next) => {
-      const note = req.body;
-      try {
-        const noteCreated = await notesService.createNote(note);
-        res.status(201).json({
-          message: 'note created',
-          data: noteCreated
-        });
-      } catch (error) {
-        next(boom.badRequest(error));
-      }
+  });
+  router.post('/', authErrorHandler, async (req, res, next) => {
+    const note = req.body;
+    try {
+      const noteCreated = await notesService.createNote(note);
+      res.status(201).json({
+        message: 'note created',
+        data: noteCreated
+      });
+    } catch (error) {
+      next(boom.badRequest(error));
     }
-  );
-  router.put(
-    '/:id',
-    passport.authenticate('jwt', { session: false }),
-    async (req, res, next) => {
-      const { id } = req.params;
-      const note = req.body;
-      try {
-        const noteUpdated = await notesService.updateNote(id, note);
-        res.json({
-          message: 'Nonte updated',
-          data: noteUpdated
-        });
-      } catch (error) {
-        next(boom.badRequest(error));
-      }
+  });
+  router.put('/:id', authErrorHandler, async (req, res, next) => {
+    const { id } = req.params;
+    const note = req.body;
+    try {
+      const noteUpdated = await notesService.updateNote(id, note);
+      res.json({
+        message: 'Nonte updated',
+        data: noteUpdated
+      });
+    } catch (error) {
+      next(boom.badRequest(error));
     }
-  );
-  router.delete(
-    '/:id',
-    passport.authenticate('jwt', { session: false }),
-    async (req, res, next) => {
-      const { id } = req.params;
-      try {
-        const noteDeleted = await notesService.deleteNote(id);
-        res.json({
-          message: 'Note deleted',
-          data: noteDeleted
-        });
-      } catch (error) {
-        next(boom.badRequest(error));
-      }
+  });
+  router.delete('/:id', authErrorHandler, async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const noteDeleted = await notesService.deleteNote(id);
+      res.json({
+        message: 'Note deleted',
+        data: noteDeleted
+      });
+    } catch (error) {
+      next(boom.badRequest(error));
     }
-  );
+  });
 }
 
 module.exports = apiNotes;
